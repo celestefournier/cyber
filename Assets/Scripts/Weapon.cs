44d27eecea417b64ce50ea766aff418f319e603d
Celeste Fournier <gustavo.s.fournier@gmail.com>
2022-06-25 20:37:43 -0300

    Add sword attack animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
 {
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            anim.SetTrigger("attack");
+        }
+    }
+
     public void SetRotation(Vector2 direction)
     {
-        print(direction);
-        float baseAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 180;
+        float baseAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Euler(0, 0, baseAngle);
     }
 
