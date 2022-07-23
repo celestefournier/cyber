ae86f2e88bf269b8eb97b509c518cb8f085b09e6
Celeste Fournier <gustavo.s.fournier@gmail.com>
2022-07-22 21:41:16 -0300

    Add laser upgrade

--- Assets/Scripts/Upgrades/Laser/LaserUpgrade.cs
+++ Assets/Scripts/Upgrades/Laser/LaserUpgrade.cs
@@ -1,7 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
 public class LaserUpgrade : UpgradeBase
 {
+    [SerializeField] LaserWeapon laserWeapon;
+
+    float fireInterval = 3;
+
     void Awake()
     {
         levelMax = 5;
+    }
+
+    public override void LevelUp()
+    {
+        if (level >= levelMax)
+            return;
+
+        level++;
+
+        if (level == 1)
+            StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(fireInterval);
+
+            for (int i = 0; i < level; i++)
+            {
+                var randomRotate = Quaternion.Euler(0, 0, Random.Range(0, 360));
+
+                Instantiate(laserWeapon, transform.position, randomRotate, transform)
+                    .Init(player, onKill);
+            }
+        }
     }
 }
