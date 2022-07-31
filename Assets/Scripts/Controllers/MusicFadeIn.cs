d5e5b87b1c06947085254c6c3a484bc6de6c7906
Celeste Fournier <celeste.fournier@hotmail.com>
2022-07-31 02:47:36 -0300

    Update gameplay and menu music

--- Assets/Scripts/Controllers/MusicFadeIn.cs
+++ Assets/Scripts/Controllers/MusicFadeIn.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class MusicFadeIn : MonoBehaviour
 {
+    [SerializeField] [Range(0f, 1f)] float volume = 1;
+
     void Start()
     {
         var audioSource = GetComponent<AudioSource>();
 
         audioSource.volume = 0;
-        audioSource.DOFade(1, 1);
+        audioSource.DOFade(volume, 1);
     }
 }
